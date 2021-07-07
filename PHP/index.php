4a15e9cbe3688c088cf6cf4d42253cd16be8452b
carlton2000 <carltonvisagie@gmail.com>
2021-07-07 12:52:30 +0200

    update

---
+++ PHP/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = "PHP";
+  
+echo "<p>This is a $name tutorial</p>";
+
+$string1 ="<p>My name is Carlton Visagie";
+$string2 ="and I am currently learning $name </p>";
+
+echo $string1." ".$string2;
+
+$myNumber = 45;
+$calculation = $myNumber * 55 / 132 + 4;
+
+echo "The result = ".$calculation;
+
+$myBool = false;
+
+echo "<p>This statement is true? ".$myBool."</p>";
+
+$variableName = "name";
+
+echo $$variableName;
+
+?> 
